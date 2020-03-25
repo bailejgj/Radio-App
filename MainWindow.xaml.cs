cb13bc3a5841bc7e614762186f75f521aad7ed75
Joshua Bailey <JBailey@spartaglobal.com>
2020-03-25 12:44:30 +0000

    Radio App project

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RadioWPFApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Radio radio = new Radio();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        //Turning the radio on or off
+        private void PowerButton_Click(object sender, RoutedEventArgs e)
+        {
+            radio.TurnOnOff();
+            powerButton.Content = FindResource(powerButton.Content == FindResource("Off") ? "On" : "Off");
+            On_Off_Text.Text = radio.Play();
+            if (radio._on == true)
+            {
+                MediaElement.Play();
+            }
+            else if (radio._on == false)
+            {
+                MediaElement.Stop();
+            }
+        }
+        //Changing to channel 1
+        private void Chan1_Click(object sender, RoutedEventArgs e)
+        {
+            
+            radio.Channel= 1;
+            MediaElement.Source = new Uri(radio.Chan1URI, UriKind.RelativeOrAbsolute);
+            On_Off_Text.Text = radio.Play();
+            MediaElement.Play();
+        }
+        //Changing to channel 2
+        private void Chan2_Click(object sender, RoutedEventArgs e)
+        {
+            radio.Channel = 2;
+            MediaElement.Source = new Uri(radio.Chan2URI, UriKind.RelativeOrAbsolute);
+            On_Off_Text.Text = radio.Play();
+            MediaElement.Play();
+        }
+        //Changing to channel 3
+        private void Chan3_Click(object sender, RoutedEventArgs e)
+        {
+            radio.Channel = 3;
+            MediaElement.Source = new Uri(radio.Chan3URI, UriKind.RelativeOrAbsolute);
+            On_Off_Text.Text = radio.Play();
+            MediaElement.Play();
+        }
+        //Changing to channel 4
+        private void Chan4_Click(object sender, RoutedEventArgs e)
+        {
+            radio.Channel = 4;
+            MediaElement.Source = new Uri(radio.Chan4URI, UriKind.RelativeOrAbsolute);
+            On_Off_Text.Text = radio.Play();
+            MediaElement.Play();
+        }
+        //Volume slider controls
+        private void VUp_Click(object sender, RoutedEventArgs e)
+        {
+            radio.Volume+=1;
+            vBox.Text = $"{radio.Volume}";
+        }
+        private void VDown_Click(object sender, RoutedEventArgs e)
+        {
+            radio.Volume--;
+            vBox.Text = $"{radio.Volume}";
+        }
+    }
+}
---
+++ Radio.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace RadioWPFApp
+{
+    public class Radio
+    {
+        public bool _on = false;
+        private int _channel = 1;
+        private int _volume = 0;
+        public string Chan1URI = "http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio1_mf_p";
+        public string Chan2URI = "http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio2_mf_p";
+        public string Chan3URI = "http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio3_mf_p";
+        public string Chan4URI = "http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio4fm_mf_p";
+        public int Channel { get { return _channel; }
+            set
+            {
+                if (_on)
+                {
+                    if (value > 0 && value <= 4)
+                    {
+                        _channel = value;
+                    }
+                }
+            }
+        }
+        public int Volume
+        {
+            get { return _volume; }
+            set
+            {
+                if (_on)
+                {
+                    if (value > 0 && value <= 100)
+                    {
+                        _volume = value;
+                    }
+                }
+            }
+        }
+
+        public bool TurnOnOff()
+        {
+            _on = !_on;
+            return _on;
+        }
+
+        public string Play()
+        {
+            if (_on) return $"Playing channel {Channel}";
+            else return "Radio is off";
+        }
+    }
+}
